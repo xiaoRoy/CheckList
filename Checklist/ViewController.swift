ce07c246f86d3250a1601a9b1fdeed5aef6229e5
Jerry Li <xiaoroyg@163.com>
2019-02-23 17:12:39 +0800

    Initial Commit

---
+++ Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by Jerry Li on 2019/2/23.
+//  Copyright © 2019 Jerry Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

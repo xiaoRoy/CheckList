73793b9b6e7450a9b9ecbd433e4dc743313c21b9
Jerry Li <xiaoroyg@163.com>
2019-02-24 18:57:36 +0800

    Put itmes intos the checklist

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    let items = ["Walk the dog", "Brush my teeth", "Learn iOS develoment",
+                 "Soccer practice", "Eat ice cream"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // MARK:- Table View Data Source
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return items.count * 20
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        
+        let lable = cell.viewWithTag(1000) as! UILabel
+        lable.text = items[indexPath.row % items.count]
+        return cell
+    }
 
 }
 

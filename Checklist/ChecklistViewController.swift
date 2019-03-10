fec67552c57f5c67c745cd17c92562cbd59fa9c4
Jerry Li <xiaoroyg@163.com>
2019-03-10 22:14:30 +0800

    Checklist item state.

--- Checklist/ChecklistViewController.swift
+++ Checklist/ChecklistViewController.swift
@@ -12,6 +12,8 @@ class ChecklistViewController: UITableViewController {
     
     let items = ["Walk the dog", "Brush my teeth", "Learn iOS develoment",
                  "Soccer practice", "Eat ice cream"]
+    
+    var itemCheckState = [false, false, false, false, false]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +23,42 @@ class ChecklistViewController: UITableViewController {
     // MARK:- Table View Data Source
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
-        return items.count * 20
+        return items.count
     }
     
     override func tableView(_ tableView: UITableView,
                             cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        
+        configureCheckMakr(for: cell, at: indexPath)
         let lable = cell.viewWithTag(1000) as! UILabel
         lable.text = items[indexPath.row % items.count]
         return cell
+    }
+    
+    // MARK:- Table View Delegate
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            let index = indexPath.row % itemCheckState.count
+            let isChecked = !itemCheckState[index]
+            itemCheckState[index] = isChecked
+            if isChecked {
+                cell.accessoryType = .checkmark
+            } else {
+                cell.accessoryType = .none
+            }
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func configureCheckMakr(for cell: UITableViewCell,
+                            at indexPath: IndexPath) {
+        let index = indexPath.row % itemCheckState.count
+        if itemCheckState[index] {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
     }
 
 }

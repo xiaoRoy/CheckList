caa3b6d509c9976322058cb3ade683406999ff2f
Jerry Li <xiaoroyg@163.com>
2020-03-14 11:50:11 +0800

    Setup ChecklistDetailViewController.

--- Checklist/AllListsViewController.swift
+++ Checklist/AllListsViewController.swift
@@ -9,18 +9,19 @@
 import UIKit
 
 
-enum ChecklistType: String {
-    case addChecklist  = "Add Checklist"
-    case editCheckList = "Edit CheckList"
+enum ChecklistDetailType: String {
+    case addChecklist  = "AddChecklist"
+    case editCheckList = "EditCheckList"
 }
 
 
-class AllListsViewController: UITableViewController {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
+   
     
     let cellIdentifier = "CheckListCell"
     
     var allLists = [Checklist]() //Array<CheckList>()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
@@ -31,6 +32,9 @@ class AllListsViewController: UITableViewController {
         if segue.identifier == "ShowChecklist" {
             let destination = segue.destination as! ChecklistViewController
             destination.checklist = sender as? Checklist
+        } else if segue.identifier == ChecklistDetailType.addChecklist.rawValue {
+            let controller = segue.destination as! ListDetailViewController
+            controller.delegate = self
         }
     }
     
@@ -40,13 +44,27 @@ class AllListsViewController: UITableViewController {
         allLists.append(Checklist(name: "Cool Apps"))
         allLists.append(Checklist(name: "To Do"))
     }
-
+    
+    //MARK:- ListDetailViewControllerDelegate
+       
+    func listDetailViewControllerDidCancel(_ controller: ListDetailViewController) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishedAdding checklist: Checklist) {
+        
+    }
+    
+    func listDetailViewController(_ controller: ListDetailViewController, didFinishedEditing checklist: Checklist) {
+        
+    }
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return allLists.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
